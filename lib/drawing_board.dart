423a1d297ee95c3d96131a5aaa7e6f53dc55a75e
wenqiang.zhang <zhangwenqiang0802@gmail.com>
2021-06-21 20:42:00 +0800

    support redo uodo

--- lib/drawing_board.dart
+++ lib/drawing_board.dart
@@ -25,10 +25,13 @@ class DrawingBoard extends StatefulWidget {
 class _DrawingBoardState extends State<DrawingBoard> {
   List<Offset?> pointList = [];
 
+  ///存放所有坐标
   List<PointIndexModel> pointListWithIndex = [PointIndexModel(0, [])];
 
-  var map = {};
+  ///原始数据 用来实现反撤销的功能
+  List<PointIndexModel> originList = [PointIndexModel(0, [])];
 
+  /// 用index来记录每一帧的绘制
   int curIndex = 0;
 
   @override
@@ -38,22 +41,61 @@ class _DrawingBoardState extends State<DrawingBoard> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            height: 56,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  width: 1,
-                  color: Colors.grey,
-                ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          child: CustomPaint(
+            painter: MyPainter(pointListWithIndex),
+            child: Listener(
+              onPointerDown: (PointerDownEvent event) {
+                // if (event.kind != PointerDeviceKind.touch) return;
+                if (pointListWithIndex.isEmpty) {
+                  pointListWithIndex.add(PointIndexModel(0, []));
+                  originList.add(PointIndexModel(0, []));
+                }
+                setState(() {});
+              },
+              onPointerMove: (PointerMoveEvent event) {
+                // if (event.kind != PointerDeviceKind.touch) return;
+                pointListWithIndex[curIndex].list.add(event.localPosition);
+                originList[curIndex].list.add(event.localPosition);
+                setState(() {});
+              },
+              onPointerUp: (PointerUpEvent event) {
+                // if (event.kind != PointerDeviceKind.touch) return;
+                curIndex++;
+                pointListWithIndex.add(PointIndexModel(curIndex, []));
+                originList.add(PointIndexModel(curIndex, []));
+                setState(() {});
+              },
+              child: Container(
+                color: Colors.transparent,
+                // child: ListView.builder(
+                //   itemBuilder: (context, index) => Container(
+                //     margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                //     width: 300,
+                //     height: 150,
+                //     color: Colors.lightGreen[100 * index],
+                //   ),
+                // ),
               ),
             ),
+          ),
+        ),
+        Positioned(
+          bottom: 20,
+          child: Container(
+            width: MediaQuery.of(context).size.width * .65,
+            height: 70,
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.title),
+                ),
                 IconButton(
                   onPressed: () {
                     setState(() {
@@ -62,15 +104,33 @@ class _DrawingBoardState extends State<DrawingBoard> {
                       if (pointListWithIndex.length > 1) {
                         pointListWithIndex.removeAt(removeIndex);
                         curIndex--;
+                      } else {
+                        logger.v(pointListWithIndex.length);
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('无法撤销'),
+                          duration: Duration(milliseconds: 500),
+                        ));
                       }
-                      logger.v(pointListWithIndex.length);
                     });
                   },
-                  icon: Icon(Icons.redo),
+                  icon: Icon(Icons.undo),
                 ),
                 IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.undo),
+                  onPressed: () {
+                    if (originList.length != pointListWithIndex.length) {
+                      var removeIndex = pointListWithIndex.length - 1;
+                      curIndex++;
+                      pointListWithIndex.insert(removeIndex, originList[removeIndex]);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('无法还原'),
+                          duration: Duration(milliseconds: 500),
+                        ),
+                      );
+                    }
+                  },
+                  icon: Icon(Icons.redo),
                 ),
                 IconButton(
                   onPressed: () {
@@ -84,38 +144,13 @@ class _DrawingBoardState extends State<DrawingBoard> {
                 ),
               ],
             ),
-          ),
-          Expanded(
-            child: CustomPaint(
-              painter: MyPainter(pointListWithIndex),
-              child: Listener(
-                onPointerDown: (PointerDownEvent event) {
-                  // if (event.kind != PointerDeviceKind.touch) return;
-                  if (pointListWithIndex.isEmpty) {
-                    pointListWithIndex.add(PointIndexModel(0, []));
-                  }
-                  setState(() {});
-                },
-                onPointerMove: (PointerMoveEvent event) {
-                  // if (event.kind != PointerDeviceKind.touch) return;
-                  // pointList.add(event.localPosition);
-                  pointListWithIndex[curIndex].list.add(event.localPosition);
-                  setState(() {});
-                },
-                onPointerUp: (PointerUpEvent event) {
-                  // if (event.kind != PointerDeviceKind.touch) return;
-                  curIndex++;
-                  pointListWithIndex.add(PointIndexModel(curIndex, []));
-                  setState(() {});
-                },
-                child: Container(
-                  color: Colors.transparent,
-                ),
-              ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(50),
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
